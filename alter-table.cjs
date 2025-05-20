1fdb176fc80dc3960153e39b6fc9537bfaab19b5
medhicorneille <medhi.famibelle@akabi.eu>
2025-05-20 23:25:46 +0200

    integrating rating

--- alter-table.cjs
+++ alter-table.cjs
@@ -9,9 +9,10 @@ const pool = new Pool({
 
 async function alterTable() {
   try {
-    const sql = `ALTER TABLE transcriptions ADD COLUMN author TEXT DEFAULT 'whisper-large-v3';`;
+    const sql = `ALTER TABLE transcriptions   ADD COLUMN rating SMALLINT     CHECK (rating BETWEEN 0 AND 5)    DEFAULT 0;`
+    //const sql = `ALTER TABLE transcriptions ADD COLUMN author TEXT DEFAULT 'whisper-large-v3';`;
     await pool.query(sql);
-    console.log('✅ Colonne "author" ajoutée avec succès.');
+    console.log('✅ Colonne ajoutée avec succès.');
   } catch (err) {
     console.error('❌ Erreur :', err.message);
   } finally {
